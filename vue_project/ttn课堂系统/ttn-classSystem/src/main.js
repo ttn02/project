dd74fc974d85889956392b857aaeb6962c5c11c3
ttn <3442322864@qq.com>
2025-03-24 18:29:59 +0800

    配置pinia

--- vue_project/ttn课堂系统/ttn-classSystem/src/main.js
+++ vue_project/ttn课堂系统/ttn-classSystem/src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router.js'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
+const pinia = createPinia()
 
 app.mount('#app')
 app.use(router)
+app.use(pinia)
--- vue_project/ttn课堂系统/ttn-classSystem/src/pages/home.vue
+++ vue_project/ttn课堂系统/ttn-classSystem/src/pages/home.vue
@@ -2,4 +2,11 @@
     <div>
         ttn课堂
     </div>
+    <span>{{ user.token }}</span>
 </template>
+
+<script setup>
+import useUserStore from "../store/user.js";
+const user = useUserStore();
+
+</script>
